8a26517dda8eab12cbe46be8b334a3bc15d3347a
Ito Fumitaka <ffffffiffffffffffff@yahoo.co.jp>
2021-05-04 09:46:23 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="space-y-20 bg-amber">
+    <header class="mb-3.5 mx-auto py-3.5 px-0 text-center">
+      <h1 class="font-normal text-3xl text-gray">ECサイト研究所</h1>
+      <p class="text-sm">ECサイトのUI/UXを研究するサイト</p>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'amber': '#FFF100',
+        'gray': '#383838',
+        }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
