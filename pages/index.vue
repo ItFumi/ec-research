caa49676929c116a1f23a2d8a7a9568cfa18b187
Ito Fumitaka <ffffffiffffffffffff@yahoo.co.jp>
2021-08-28 19:27:32 +0900

    ページネーション実装

--- pages/index.vue
+++ pages/index.vue
@@ -5,13 +5,14 @@
       <p class="text-sm">ECサイトのUI/UXを研究するサイト</p>
     </header>
     <div class="flex flex-wrap px-5 mx-auto">
-      <div class="animate-contentsFadeIn md:w-1/3 p-2" v-for="(e, i) in siteItems" :style="{ 'animation-delay': `${i * 0.2}s` }">
-        <a :href="e.url" class="bg-gray block p-4 rounded-2xl">
+      <div class="animate-contentsFadeIn md:w-1/3 p-2" v-for="(e, i) in siteItems.slice(6*(this.currentPage-1), 6*this.currentPage)" :style="{ 'animation-delay': `${i * 0.2}s` }">
+        <a :href="e.url" class="bg-gray block p-4 rounded-2xl" target="_blank" rel="noopener noreferrer">
           <img class="rounded-2xl" :src="e.image.url">
           <span class="animate-fadeInLeft block mt-2.5 font-bold text-white text-2xl" :style="{ 'animation-delay': `${i * 0.2}s` }">{{e.title}}</span>
         </a>
       </div>
     </div>
+    <paginate :page-count="getPageCount" :click-handler="clickCallback"></paginate>
     <footer class="bg-gray mt-3.5 p-7 relative rounded-t-2xl text-white">
       <small>Copyright &copy; 2021 ItFumi</small>
     </footer>
@@ -20,11 +21,14 @@
 
 <script>
 import axios from 'axios'
+
 export default {
   data() {
     return {
       // APIを叩いて返ってきたコンテンツを格納する用に空の配列を用意
       siteItems: [],
+      parPage: 6,
+      currentPage: 1,
     }
   },
   mounted() {
@@ -33,27 +37,33 @@ export default {
   },
   methods: {
     getSites() {
-      // Git管理しないAPIキーは、.envファイル内にて別途定義する
       // オプションとして、コンテンツの最大取得件数は100件と設定
-      axios.get('https://ec-research.microcms.io/api/v1/ecsite', {
+      axios.get('https://ec-research.microcms.io/api/v1/ecsite?', {
           headers: { 'X-API-KEY': this.$config.apiKEY }
         })
         .then((res) => {
           // 取得したコンテンツをコンポーネントのdata内に格納
           this.siteItems = res.data.contents
-          // 取得したアイテムをシャッフル
-          for (let i = this.siteItems.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1))
-            const tmp = this.siteItems[i]
-            this.siteItems[i] = this.siteItems[j]
-            this.siteItems[j] = tmp
-          }
         })
         .catch((err) => {
           console.log(err)
         })
+    },
+    clickCallback: function (pageNum) {
+      this.currentPage = Number(pageNum)
+      this.siteItems.length = 0
     }
-  }
+  },
+  computed: {
+    siteItems: function() {
+      let current = this.currentPage * this.parPage
+      let start = current - this.parPage
+      return this.siteItems.slice(start, current)
+    },
+    getPageCount: function() {
+      return Math.ceil(this.siteItems.length / this.parPage)
+    }
+   }
 }
 </script>
 
